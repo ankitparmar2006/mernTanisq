968d2f84b77421a8cbea0747776d7bf7679587c0
ankitparmar2006 <ankitparmar30202@gmail.com>
2025-05-03 05:14:48 +0530

    no work ony file change

--- backend/models/empModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose= require("mongoose");
-const empSchema= new mongoose.Schema({ 
-   empno:String,
-   name:String,
-   designation:String, 
-   salary:Number
-})
-module.exports = mongoose.model("employee", empSchema);
